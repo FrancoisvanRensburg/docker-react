087c787579cfca2edc1aae09560a0fa8247c95ad
Francois van Rensburg <jvrensburgfrans@gmail.com>
2020-08-17 18:01:47 +0200

    Added travis and updates App.js to pass tests

--- src/App.js
+++ src/App.js
@@ -10,7 +10,15 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        yes testing again - 1
+        <a
+          className='App-link'
+          href='https://reactjs.org'
+          target='_blank'
+          rel='noopener noreferrer'
+        >
+          Learn React
+        </a>
+        <p>Change one</p>
       </header>
     </div>
   );
